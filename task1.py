6b9f67153a99f1f14daffc1469f39142c1ea2d4c
Dhruv Mishra <125127954+Philioraptor@users.noreply.github.com>
2026-01-03 07:54:07 +0000

    final commit Task1 and task 2 solutions added Task 1: basic python arithmetic operations Task 2: create a personalized greeting
    'origin/main'.
    nothing

---
+++ task1.py
@@ -0,0 +1,20 @@
+#Task 1: Basic Mathematical Operations on Two Numbers
+
+#two numbers as X and Y
+
+x = input("Enter the first number:")
+y = input("Enter the Second number:")
+
+#basic mathematical operations.....
+#Addition
+add = float(x) + float(y)
+print("the addition of numbers is:", add)
+#Subtraction
+sub = float(x) - float(y)
+print("the subtraction of numbers is:", sub)
+#Multiply
+mul = float(x) * float(y)
+print("the multiplication of numbers is:", mul)
+#Divide
+div = float(x) / float(y)
+print("the division of numbers is:", div)
---
+++ task2.py
@@ -0,0 +1,8 @@
+#Task 2: Create a Personalized Greeting
+#Get user's name
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name:")
+
+#create a personalized greeting message
+
+print("hello", firstname, lastname + "! Welcome to the Python Program. ")
