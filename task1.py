a04922a186e63f53767ba5213a948322894656b7
Dhruv Mishra <125127954+Philioraptor@users.noreply.github.com>
2026-01-10 12:29:30 +0530

    Bug handling

--- task1.py
+++ task1.py
@@ -1,13 +1,12 @@
 #Task 1: Check if a Number is Even or Odd
 
 
-x = input("Enter the Number:")
+x = int(input("Enter the Number:"))
 
-y= x%2
 #checking the number is even or odd
-if(y==0):
+if(x%2==0):
   print(f"The Entered number {x} is even. ")
-elif:
+elif(x%2!=0):
   print(f"The Entered number {x} is odd. ")
 else:
   print("Invalid Input. Please try again")
