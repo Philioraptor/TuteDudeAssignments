8b7020aea850f8ef3d694efdc916b4cdc3fdd779
Dhruv Mishra <125127954+Philioraptor@users.noreply.github.com>
2026-01-11 21:25:04 +0530

    Replace greeting with sum of integers calculation

--- task2.py
+++ task2.py
@@ -1,8 +1,11 @@
-#Task 2: Create a Personalized Greeting
-#Get user's name
-firstname = input("Enter your first name: ")
-lastname = input("Enter your last name:")
+#Task 2: Sum of Integers from 1 to 50 Using a Loop
+sum = 0
+#1.   Uses a for loop to iterate over numbers from 1 to 50.
+for i in range(1,51):
+#2.   Calculates the sum of all integers in this range.
+  sum = sum + i
 
-#create a personalized greeting message
 
-print("hello", firstname, lastname + "! Welcome to the Python Program. ")
+#3.   Displays the final sum.
+print(f"The sum of all the integers from 1 to 50 is {sum}")
+
