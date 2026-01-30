73a94fe7e8c7db2571777f23a2de84c1a51f893d
Dhruv Mishra <125127954+Philioraptor@users.noreply.github.com>
2026-01-30 17:14:45 +0530

    Implement math calculations using user input
    Replaced the sum calculation with a program that uses the math module to compute the square root, natural logarithm, and sine of a user-provided number.

--- task2.py
+++ task2.py
@@ -1,11 +1,31 @@
-#Task 2: Sum of Integers from 1 to 50 Using a Loop
-sum = 0
-#1.   Uses a for loop to iterate over numbers from 1 to 50.
-for i in range(1,51):
-#2.   Calculates the sum of all integers in this range.
-  sum = sum + i
+"""
+Task 2: Using the Math Module for Calculations
+ 
+Problem Statement: Write a Python program that:
+1.   Asks the user for a number as input.
+2.   Uses the math module to calculate the:
+o   Square root of the number
+o   Natural logarithm (log base e) of the number
+o   Sine of the number (in radians)
+3.   Displays the calculated results.
+
+"""
+
+import math
+
+def math_calc(num):
+  sqrt_n = math.sqrt(num)
+  log_n = math.log(num)
+  sin_n = math.sin(num)
+  return sqrt_n, log_n, sin_n
+  
+#please enter the number
+number = float(input("Enter a Number:"))
+               
+#calling the function
+sqrt_n, log_n, sin_n = math_calc(number)
+
+print(f"the squareroot of {number} is: {sqrt_n} \n The log of {number} is: {log_n} \n The Sine of {number} is: {sin_n}")
 
 
-#3.   Displays the final sum.
-print(f"The sum of all the integers from 1 to 50 is {sum}")
-
+  
